3fbc1c272ca6af8e5abe0a4095a57ee6152890e4
Daniel Mantovani <daniel.mantovani047@gmail.com>
2024-05-07 09:12:08 -0300

    feat: CSCAN finalizado

--- src/cscan/CSCAN.java
+++ src/cscan/CSCAN.java
@@ -1,53 +1,54 @@
 package src.cscan;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CSCAN {
 
-	public void run(int sectorSize, int tracks, int sectorsPerTrack, int transferRate, double seekTime, double rotationTime, ArrayList<Integer> requests, int head) {
-
+	public double run(int sectorSize, int tracks, int sectorsPerTrack, int transferRate, double seekTime, double rotationTime, ArrayList<Integer> requests, int head) {
+		Collections.sort(requests);
 		double transferTime = (sectorSize * Math.pow(2, 10)) / (transferRate * Math.pow(2, 20));
 
 		double totalSeekTime = 0;
 		double totalRotationTime = 0;
 		double totalTransferTime = 0;
 
-		int headTrack = head / sectorsPerTrack;
-		for (int i = 0; i < requests.size(); i++) {
-			int sector = requests.get(i);
-			if (head < sector)
-				continue;
+		while (!requests.isEmpty()) {
+			 int headTrack = head / sectorsPerTrack;
+			 int nextSector = this.getNextRequestInCurrentTrack(head, sectorsPerTrack, requests);
 
-			int sectorTrack = sector / sectorsPerTrack;
+            if (nextSector != -1) {
+                totalRotationTime += this.updateTotalRotationTime(head, nextSector, sectorsPerTrack, rotationTime);
+                totalTransferTime += transferTime;
+				head = nextSector;
+            }
+            if (!requests.isEmpty() && headTrack < tracks - 1) {
+                head += sectorsPerTrack;
+                totalSeekTime += seekTime;
+            }
+            else if (!requests.isEmpty() && headTrack == tracks - 1) {
+                head = head - (headTrack * sectorsPerTrack);
+                totalSeekTime += seekTime * headTrack;
+            }
+        }
+		return totalSeekTime + totalRotationTime + totalTransferTime;
+	}
 
-			if (headTrack < sectorTrack) {
-				totalSeekTime += seekTime;
-				head += sectorsPerTrack;
-			} else if (headTrack > sectorTrack) {
-				totalSeekTime += seekTime;
-				head -= sectorsPerTrack;
+	private int getNextRequestInCurrentTrack(int head, int sectorsPerTrack, ArrayList<Integer> requests) {
+		for (Integer request : requests) {
+			if ((request / sectorsPerTrack) == (head / sectorsPerTrack)) {
+				requests.remove(request);
+				return request;
 			}
-			headTrack = sectorTrack;
-
-			totalRotationTime += ((double) Math.abs(head - sector) / sectorsPerTrack) * rotationTime;
-			totalTransferTime += transferTime;
-
-			head = sector;
-			requests.remove(i);
-
-			if (requests.isEmpty())
-				break;
-
-			if (headTrack < tracks - 1) {
-				head += sectorsPerTrack;
-			} else if (headTrack == tracks - 1) {
-				i = 0;
-				head = head - (headTrack * sectorsPerTrack);
-				headTrack = head / sectorsPerTrack;
-			}
-
 		}
+		return -1;
+	}
 
+	private double updateTotalRotationTime(int head, int nextSector, int sectorsPerTrack, double rotationTime) {
+		if (head <= nextSector)
+            return ((double) (nextSector - head) / sectorsPerTrack) * rotationTime;
+		else
+			return ((double) ((sectorsPerTrack - head % sectorsPerTrack) + nextSector % sectorsPerTrack) / sectorsPerTrack) * rotationTime;
 	}
 
 }
