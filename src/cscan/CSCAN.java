9246c102c2ac2f22c21477b313035dbad5bf133d
Daniel Mantovani <daniel.mantovani047@gmail.com>
2024-05-07 06:46:08 -0300

    fix: CSCAN

--- src/cscan/CSCAN.java
+++ src/cscan/CSCAN.java
@@ -4,37 +4,48 @@ import java.util.ArrayList;
 
 public class CSCAN {
 
-	public void run(int sectorSize, int tracks, int sectorPerTrack, double seekTime, double rotationTime, double transferTime, ArrayList<Integer> requests, int head) {
+	public void run(int sectorSize, int tracks, int sectorsPerTrack, int transferRate, double seekTime, double rotationTime, ArrayList<Integer> requests, int head) {
+
+		double transferTime = (sectorSize * Math.pow(2, 10)) / (transferRate * Math.pow(2, 20));
 
 		double totalSeekTime = 0;
 		double totalRotationTime = 0;
 		double totalTransferTime = 0;
 
-		int headTrack = head / sectorPerTrack;
+		int headTrack = head / sectorsPerTrack;
 		for (int i = 0; i < requests.size(); i++) {
 			int sector = requests.get(i);
 			if (head < sector)
 				continue;
 
-			int sectorTrack = sector / sectorPerTrack;
+			int sectorTrack = sector / sectorsPerTrack;
 
 			if (headTrack < sectorTrack) {
 				totalSeekTime += seekTime;
-				head += sectorPerTrack;
+				head += sectorsPerTrack;
 			} else if (headTrack > sectorTrack) {
 				totalSeekTime += seekTime;
-				head -= sectorPerTrack;
+				head -= sectorsPerTrack;
 			}
 			headTrack = sectorTrack;
 
-			totalRotationTime += ((double) Math.abs(head - sector) / sectorPerTrack) * rotationTime;
+			totalRotationTime += ((double) Math.abs(head - sector) / sectorsPerTrack) * rotationTime;
+			totalTransferTime += transferTime;
+
 			head = sector;
 			requests.remove(i);
 
-			if (i == requests.size()) {
+			if (requests.isEmpty())
+				break;
+
+			if (headTrack < tracks - 1) {
+				head += sectorsPerTrack;
+			} else if (headTrack == tracks - 1) {
 				i = 0;
-				head = head - (headTrack * sectorPerTrack);
+				head = head - (headTrack * sectorsPerTrack);
+				headTrack = head / sectorsPerTrack;
 			}
+
 		}
 
 	}
