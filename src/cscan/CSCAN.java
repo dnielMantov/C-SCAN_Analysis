2e2b421ba0b6f1382e864430d0b9495c40ca4726
Daniel Mantovani <daniel.mantovani047@gmail.com>
2024-05-07 18:28:03 -0300

    refact: parâmetros fornecidos

--- src/cscan/CSCAN.java
+++ src/cscan/CSCAN.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 
 public class CSCAN {
 
-	public double run(int sectorSize, int tracks, int sectorsPerTrack, int transferRate, double seekTime, double rotationTime, int head, ArrayList<Integer> requests) {
+	public double run(int sectorSize, int tracks, int sectorsPerTrack, int transferRate, double seekTime, double rotationTime, int head, ArrayList<Integer> requests, boolean printSectors) {
 		ArrayList<Integer> accessedSectors = new ArrayList<>();
 		double transferTime = (sectorSize * Math.pow(2, 10)) / (transferRate * Math.pow(2, 20));
 
@@ -36,7 +36,8 @@ public class CSCAN {
 				}
 			}
 		}
-		System.out.println("\nAccessed sectors:\n" + accessedSectors);
+		if (printSectors)
+			System.out.println("\nAccessed sectors:\n" + accessedSectors);
 		return totalSeekTime + totalRotationTime + totalTransferTime;
 	}
 
--- src/cscan/RunCSCAN.java
+++ src/cscan/RunCSCAN.java
@@ -16,10 +16,12 @@ public class RunCSCAN {
         int base                = 0;        // initial number of the range
         int bounds              = 35;       // last number of the range
         int number              = 20;       // number of requests
+        boolean printRequests   = true;     // Print the random requests
 
 
         ArrayList<Integer> requests = Utils.getRandomRequests(base, bounds, number);
-        System.out.println("Requests:\n" + requests);
+        if (printRequests)
+            System.out.println("Requests:\n" + requests);
 
 
         // Select parameters
@@ -30,9 +32,10 @@ public class RunCSCAN {
         double seekTime         = 4;        // R/W head seek time in ms
         double rotationTime     = 2;        // disk rotation time in ms
         int initialSector       = 21;       // initial position of disk arm's head
+        boolean printSectors    = true;     // Print the accessed sectors
 
 
-        double totalTime = cscan.run(sectorSize, tracks, sectorsPerTrack, transferRate, seekTime, rotationTime, initialSector, requests);
+        double totalTime = cscan.run(sectorSize, tracks, sectorsPerTrack, transferRate, seekTime, rotationTime, initialSector, requests, printSectors);
         System.out.println(df.format(totalTime) + " ms");
     }
 
